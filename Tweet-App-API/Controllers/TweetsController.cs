5a5593f0f015d3b68ab901acf674e4c418477733
cogdotnet410 <cogdotnet410@CTSDOTNET0198>
2022-05-12 19:59:51 +0530

    Add project files.

---
+++ Tweet-App-API/Controllers/TweetsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweet_App_API.Model;
+
+namespace Tweet_App_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TweetsController : ControllerBase
+    {
+        private readonly ILogger<TweetsController> _logger;
+
+        public static List<User> userList = new List<User>();
+
+        public TweetsController(ILogger<TweetsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Register")]
+        public List<User> Register(User user)
+        {
+            userList.Add(user);
+            return userList;
+        }
+
+        [HttpGet]
+        public List<User> GetAll()
+        {
+            return userList;
+        }
+
+    }
+}
